6890b57a5737c4e83974283573511d551a846f39
Getnetmulat <Getnetmulat>
2023-10-15 13:55:25 +0300

    EICHRMS Commit

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,4 @@
 from App import app
 
 if __name__ == "__main__":
-    app.run(host='localhost', debug='False', port=int("80"))
+    app.run(host='localhost', debug='False', port=int("80")) #port 80 is the default port you can change what you can you use
